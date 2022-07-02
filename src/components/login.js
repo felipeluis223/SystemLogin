bd9c1ba31fdf92a3b70d763918295bb87cc17b54
felipeluis223 <lf9072362@gmail.com>
2022-07-02 19:01:03 -0300

    campos de email/senha

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,24 @@
+import { BsPersonCircle }from 'react-icons/bs'
+import TextField from '@mui/material/TextField';
+import ReactDOM from 'react-dom';
 import './styles/login.css'
+import '../index.css'
 
 function Login(){
     return (
         <div className="containerLogin">
-            <h2>Container de Login</h2>
+         	<div className="">
+            {/*    <BsPersonCircle /> */}
+                <TextField className="text-3xl font-bold text-white" id="outlined-basic" label="E-mail" variant="outlined" />
+            </div>
+            
+			<div className="">
+            {/*    <BsPersonCircle /> */}
+                <TextField className="text-3xl font-bold text-white" id="outlined-basic" label="E-mail" variant="outlined" />
+            </div>
+            <h2 className="text-white">Ola</h2>
+            
+
         </div>
     )
 }
