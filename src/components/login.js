8f43e4f78261a2377ff4e332a92cfc5c1191ec7b
felipeluis223 <lf9072362@gmail.com>
2022-07-02 18:01:00 -0300

    Estrutura da layout do Login

---
+++ src/components/login.js
@@ -0,0 +1,11 @@
+import './styles/login.css'
+
+function Login(){
+    return (
+        <div className="containerLogin">
+            <h2>Container de Login</h2>
+        </div>
+    )
+}
+
+export default Login;
