bef582d4c0e9a85a7224055dbcaf9583722d7a2f
felipeluis223 <lf9072362@gmail.com>
2022-07-04 21:56:50 -0300

    alterações background

--- src/components/cadastro.js
+++ src/components/cadastro.js
@@ -2,12 +2,21 @@ import TextField from '@mui/material/TextField';
 import { AiOutlineLogin } from "react-icons/ai";
 import Button from '@mui/material/Button';
 import ReactDOM from 'react-dom';
+import React, { useState } from "react";
 import './styles/login.css'
 
 function Cadastro(){
+	const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+
+	const [errorText, setErrorText] = useState('')
+	const [nomeUser, setNomeUser] = useState('')
+	const [telefoneUser, setTelefoneUser] = useState('');
+	const [emailUser, setEmailUser] = useState('')
+
+
+
     return (
         <div className="containerLogin flex justify-center flex-wrap absolute">
-
         	<div className="w-[100%] flex flex-row mb-3.5 text-5xl text-sky-500 flex-row flex-wrap">
         		<AiOutlineLogin />
 	        	<h1>Cadastrar</h1>
@@ -15,13 +24,15 @@ function Cadastro(){
 	        </div>
 	        <form action="">
 	        	<div className="w-[100%] flex flex-row justify-evenly flex-wrap">
-		         	<div className="w-[48%] mt-5">
+		         	<div className="w-[96%] mt-5">
 		         		<TextField
 				          required
-				          id="outlined"
-				          label="Nome"
+				          id="standard-basic" label="Nome"
 				          placeholder="digite seu nome..."
 				          className="w-[100%]"
+				          onChange={(e)=>setNomeUser(e.target.value)}
+				          error={nomeUser === ""}
+				          helperText={nomeUser === ""?"Campo vazio":''}
 				        />
 		         	</div>
 
@@ -32,6 +43,9 @@ function Cadastro(){
 				          label="Telefone"
 				          placeholder="(dd)xxxxx-xxxx"
 				          className="w-[100%]"
+				          onChange={(e)=>setTelefoneUser(e.target.value)}
+				          error={telefoneUser === ""}
+				          helperText={telefoneUser === ""?"Campo vazio":''}
 				        />
 		         	</div>
 
@@ -42,19 +56,11 @@ function Cadastro(){
 				          label="E-mail"
 				          placeholder="exemple@gmail.com"
 				          className="w-[100%]"
+				          onChange={(e)=>setEmailUser(e.target.value)}
+				          error={setEmailUser === ""}
+				          helperText={emailUser === ""?"Campo vazio":''}
 				        />
 		         	</div>
-
-		         	<div className="w-[48%] mt-5">
-		         		<TextField
-				          required
-				          id="outlined"
-				          label="Password"
-				          placeholder="crie uma senha..."
-				          className="w-[100%]"
-				        />
-		         	</div>
-
 		         </div>
 		         <div className="w-[100%] flex justify-center align-center mt-5">
 		         	<Button variant="contained">Finish</Button>
